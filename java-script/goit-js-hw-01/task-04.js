18c3b12b716b34cad69b0254b2d32bb561b6e974
kristina.lobanets <kristina.malitowska@gmail.com>
2020-05-11 20:18:33 +0300

    ааа

---
+++ java-script/goit-js-hw-01/task-04.js
@@ -0,0 +1,22 @@
+"use strict";
+
+const credits = 23580;
+const pricePerDroid = 3000;
+
+const userRequest = prompt("Сколько дроидов хотите купить");
+const totalPrice = Number(userRequest) * pricePerDroid;
+const restCredits = credits - totalPrice;
+
+let message;
+
+if (userRequest === null) {
+  message = "Отменено пользователем!";
+} else if (totalPrice > credits) {
+  message = "Недостаточно средств на счету!";
+} else {
+  message = `Вы купили ${userRequest} дроидов, на счету осталось ${restCredits} кредитов.`;
+}
+
+console.log(message);
+
+alert(message);
