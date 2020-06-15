2d06a3145dcdc383a684a8dfcde7aaaed7c57b10
kristina.lobanets <kristina.malitowska@gmail.com>
2020-06-15 10:00:51 +0300

    555

--- java-script/goit-js-hw-03/tasks/task-01.js
+++ java-script/goit-js-hw-03/tasks/task-01.js
@@ -4,21 +4,11 @@
 
 //Напиши скрипт, который, для объекта user, последовательно:
 
-const user = {
-  name: "Mango",
-  age: 20,
-  hobby: "html",
-  premium: true,
-};
 //добавляет поле mood со значением 'happy'
-user.mood = "happy";
+
 //заменяет значение hobby на 'skydiving'
-user.hobby = "skydiving";
+
 //заменяет значение premium на false
-user.premium = "false";
+
 //выводит содержимое объекта user в формате ключ:значение используя Object.keys() и for...of
 
-const values = Object.keys(user);
-for (const elem of values) {
-  console.log(`${elem} : ${user[elem]}`);
-}
