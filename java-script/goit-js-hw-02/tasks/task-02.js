c7081a3d6def18c60616ff510a04b119e1025760
kristina.lobanets <kristina.malitowska@gmail.com>
2020-06-17 15:01:02 +0300

    555

--- java-script/goit-js-hw-02/tasks/task-02.js
+++ java-script/goit-js-hw-02/tasks/task-02.js
@@ -1,1 +1,31 @@
 "use strict";
+
+const calculateEngravingPrice = function (message, pricePerWord) {
+  const newMessage = message.split(" ");
+  const lenMessage = newMessage.length;
+  const result = lenMessage * pricePerWord;
+  console.log(result);
+  return message;
+};
+
+console.log(
+  calculateEngravingPrice(
+    "Proin sociis natoque et magnis parturient montes mus",
+    10
+  )
+); // 80
+
+console.log(
+  calculateEngravingPrice(
+    "Proin sociis natoque et magnis parturient montes mus",
+    20
+  )
+); // 160
+
+console.log(
+  calculateEngravingPrice("Donec orci lectus aliquam est magnis", 40)
+); // 240
+
+console.log(
+  calculateEngravingPrice("Donec orci lectus aliquam est magnis", 20)
+); // 120
