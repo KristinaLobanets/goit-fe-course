69dc30c22c458892bebe651c362afc8b78b46d21
kristina.lobanets <kristina.malitowska@gmail.com>
2020-06-17 17:25:37 +0300

    yyy

--- java-script/goit-js-hw-02/tasks/task-03.js
+++ java-script/goit-js-hw-02/tasks/task-03.js
@@ -1,16 +1,30 @@
 "use strict";
 
-Напиши фукцнию findLongestWord(string), которая принимает параметром произвольную строку (в строке будут только слова и пробелы) и возвращает самое длинное слово в этой строке.
-
-const findLongestWord = function(string) {
-  // твой код
+const findLongestWord = function (string) {
+  const newString = string.split(" ");
+  let bigWord = "";
+  for (const word of newString) {
+    if (word.length > bigWord.length) {
+      bigWord = word;
+    }
+  }
+  return bigWord;
 };
 
-/*
- * Вызовы функции для проверки работоспособности твоей реализации.
- */
-console.log(findLongestWord('The quick brown fox jumped over the lazy dog')); // 'jumped'
+console.log(findLongestWord("The quick brown fox jumped over the lazy dog")); // 'jumped'
 
-console.log(findLongestWord('Google do a roll')); // 'Google'
+console.log(findLongestWord("Google do a roll")); // 'Google'
 
-console.log(findLongestWord('May the force be with you')); // 'force'
+console.log(findLongestWord("May the force be with you")); // 'force'
+
+// "use strict";
+
+// const findLongestWord = function (string) {
+// const newString = string.split(" ");
+//   let word = "";
+//   for (let i = 0; i < newString.length; i += 1) {
+//     if (word.length < newString[i].length) {
+//       word = newString[i];
+//     }
+//   }
+//   return word;
