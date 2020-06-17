c24755381b50f303ea86a1aaa64225d6740263c3
kristina.lobanets <kristina.malitowska@gmail.com>
2020-06-17 14:51:14 +0300

    rrr

--- java-script/goit-js-hw-02/tasks/task-01.js
+++ java-script/goit-js-hw-02/tasks/task-01.js
@@ -1,3 +1,14 @@
 "use strict";
 
+const names = ["Mango", "Poly", "Ajax", "Lux", "Jay", "Kong"];
+const numbers = [5, 10, 15, 20, 25, 30, 35, 40, 45, 50];
 
+const logItems = function (array) {
+  for (let i = 0; i < array.length; i += 1) {
+    const number = i + 1;
+    console.log(`${number} - ${array[i]}`);
+  }
+};
+
+logItems(names);
+logItems(numbers);
