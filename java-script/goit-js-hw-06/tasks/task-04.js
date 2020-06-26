a0b5ca4b9cc24ae4a48ecab5b0e10581e18001a7
kristina.lobanets <kristina.malitowska@gmail.com>
2020-06-26 18:08:51 +0300

    55555

--- java-script/goit-js-hw-06/tasks/task-04.js
+++ java-script/goit-js-hw-06/tasks/task-04.js
@@ -3,7 +3,7 @@ import users from "./module.js";
 //  пользователей (поле isActive).
 
 const getInactiveUsers = (users) => {
-  // return (inactiveUsers = users.filter((user) => !user.isActive));
+  return users.filter((user) => !user.isActive);
 };
 
 console.log(getInactiveUsers(users)); // [объект Moore Hensley, объект Ross Vazquez, объект Blackburn Dotson]
