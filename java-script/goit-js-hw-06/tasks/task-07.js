6998dd8971e84ffd70f1dbe5202f86e488d668d2
kristina.lobanets <kristina.malitowska@gmail.com>
2020-06-26 19:02:10 +0300

    555

--- java-script/goit-js-hw-06/tasks/task-07.js
+++ java-script/goit-js-hw-06/tasks/task-07.js
@@ -2,7 +2,7 @@ import users from "./module.js";
 // Получить общую сумму баланса (поле balance) всех пользователей.
 
 const calculateTotalBalance = (users) => {
-  // твой код
+  return users.reduce((akk, user) => akk + user.balance, 0);
 };
 
 console.log(calculateTotalBalance(users)); // 20916
