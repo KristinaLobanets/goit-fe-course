14e5a08c9121dcf0ba8cc811cebd6fcabfb7c2fc
kristina.lobanets <kristina.malitowska@gmail.com>
2020-06-27 16:14:05 +0300

    1234567

--- java-script/goit-js-hw-06/tasks/task-08.js
+++ java-script/goit-js-hw-06/tasks/task-08.js
@@ -2,7 +2,9 @@ import users from "./module.js";
 // Массив имен всех пользователей у которых есть друг с указанным именем.
 
 const getUsersWithFriend = (users, friendName) => {
-  // твой код
+  return users
+    .filter((user) => user.friends.includes(friendName))
+    .map((user) => user.name);
 };
 
 console.log(getUsersWithFriend(users, "Briana Decker")); // [ 'Sharlene Bush', 'Sheree Anthony' ]
