523e5cf5fa311efd23a846756aa990e2e531b09e
kristina.lobanets <kristina.malitowska@gmail.com>
2020-06-27 16:51:42 +0300

    9999

--- java-script/goit-js-hw-06/tasks/task-09.js
+++ java-script/goit-js-hw-06/tasks/task-09.js
@@ -4,8 +4,13 @@ import users from "./module.js";
 //  в зависимости от количества их друзей (поле friends)
 
 const getNamesSortedByFriendsCount = (users) => {
-  // твой код
+  const sortFriends = (a, b) => a.friends.length - b.friends.length;
+  return users.sort(sortFriends).map((user) => user.name);
 };
 
 console.log(getNamesSortedByFriendsCount(users));
 // [ 'Moore Hensley', 'Sharlene Bush', 'Elma Head', 'Carey Barr', 'Blackburn Dotson', 'Sheree Anthony', 'Ross Vazquez' ]
+
+// numbers.sort();
+// .map(user => user.name);
+// users.sort(users.frends.length);
