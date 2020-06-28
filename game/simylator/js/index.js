efa5f7898ea68ef485c8b054d8e1c9741df8a1c6
kristina.lobanets <kristina.malitowska@gmail.com>
2020-06-28 15:09:00 +0300

    yyy

---
+++ game/simylator/js/index.js
@@ -0,0 +1,22 @@
+const info__health = document.querySelector(".info__health");
+console.dir(info__health);
+const start = document.querySelector(".start");
+add_info.addEventListener("click", addInfo);
+
+
+
+
+
+// const info = document.querySelector(".info");
+// const add_info = document.querySelector(".add_info");
+
+// start.addEventListener("click", reduceInfo);
+// console.dir(info.innerText);
+// function reduceInfo() {
+//   info.innerText = Number(info.innerText) - 5;
+//   setTimeout(reduceInfo, 1000);
+// }
+
+// function addInfo() {
+//   info.innerText = Number(info.innerText) + 100;
+}
