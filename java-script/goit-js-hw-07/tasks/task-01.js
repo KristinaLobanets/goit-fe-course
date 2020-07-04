834197ced83bea3acaa0116588ae2c4e62c75abe
kristina.lobanets <kristina.malitowska@gmail.com>
2020-07-04 23:03:59 +0300

    ttttt

--- java-script/goit-js-hw-07/tasks/task-01.js
+++ java-script/goit-js-hw-07/tasks/task-01.js
@@ -6,26 +6,22 @@
 // ul#categories, то есть элементов li.item.
 // Получится 'В списке 3 категории.'.
 
-const ulCategories = Array.from(
-  document.querySelectorAll("ul#categories>li.item")
-);
-console.log(`В списке ${ulCategories.length} категории`);
+const categories = document.querySelector("#categories");
+
+console.log(`В списке ${categories.childElementCount} категории.`);
 
 // Для каждого элемента li.item в списке ul#categories,
 // найдет и выведет в консоль текст заголовка элемента
 //  (тега h2) и количество элементов в категории
 // (всех вложенных в него элементов li).
 
-const liCategories = Array.from(
-  document.querySelectorAll("ul#categories li.item ul")
-);
+const item = document.querySelectorAll(".item");
 
-const items = document.querySelectorAll(".item");
-Array.prototype.forEach.call(items, (element) => {
-  const title = element.querySelector("h2").innerHTML;
-  const itemsLength = element.querySelectorAll("li").length;
-  console.log(`Категория: ${title} / Количество элементов: ${itemsLength}`);
-});
+item.forEach((it) =>
+  console.log(
+    `Категория: ${it.firstElementChild.textContent}  Количество элементов: ${it.lastElementChild.childElementCount}`
+  )
+);
 
 // Например для первой категории получится:
 
