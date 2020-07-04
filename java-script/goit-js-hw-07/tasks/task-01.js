5314b6d67b5ef7f15e5b7d7865972a5b9930aa48
kristina.lobanets <kristina.malitowska@gmail.com>
2020-07-04 23:39:17 +0300

    hgfds

--- java-script/goit-js-hw-07/tasks/task-01.js
+++ java-script/goit-js-hw-07/tasks/task-01.js
@@ -1,29 +1,8 @@
-// В HTML есть список категорий ul#categories.
-
-// Напиши скрипт, который выполнит следующие операции.
-
-// Посчитает и выведет в консоль количество категорий в
-// ul#categories, то есть элементов li.item.
-// Получится 'В списке 3 категории.'.
-
 const categories = document.querySelector("#categories");
-
 console.log(`В списке ${categories.childElementCount} категории.`);
-
-// Для каждого элемента li.item в списке ul#categories,
-// найдет и выведет в консоль текст заголовка элемента
-//  (тега h2) и количество элементов в категории
-// (всех вложенных в него элементов li).
-
 const item = document.querySelectorAll(".item");
-
 item.forEach((it) =>
   console.log(
     `Категория: ${it.firstElementChild.textContent}  Количество элементов: ${it.lastElementChild.childElementCount}`
   )
 );
-
-// Например для первой категории получится:
-
-// Категория: Животные
-// Количество элементов: 4
--- java-script/goit-js-hw-07/tasks/task-02.js
+++ java-script/goit-js-hw-07/tasks/task-02.js
@@ -1,11 +1,3 @@
-// Задание 2
-
-// В HTML есть пустой список
-//  ul#ingredients.
-
-// <ul id="ingredients"></ul>
-// В JS есть массив строк.
-
 const ingredients = [
   "Картошка",
   "Грибы",
@@ -17,15 +9,13 @@ const ingredients = [
 
 const ingredientsList = document.querySelector("#ingredients");
 
-for (let ingridient of ingredients) {
-  let ingredientOne = document.createElement("li");
-  ingredientOne.textContent = ingridient;
-  ingredientsList.appendChild(ingredientOne);
-}
+// for (let ingridient of ingredients) {
+//   let ingredientOne = document.createElement("li");
+//   ingredientOne.textContent = ingridient;
+//   ingredientsList.appendChild(ingredientOne);
+// }
+
+const libery = ingredients.reduce((acc, item) => acc + `<li>${item}</li>`, "");
+ingredientsList.innerHTML = libery;
 
 console.log(ingredientsList);
-
-// Напиши скрипт, который для каждого элемента массива
-// ingredients создаст отдельный li, после чего вставит
-//  все li за одну операцию в список ul.ingredients.
-//   Для создания DOM-узлов используй document.createElement().
