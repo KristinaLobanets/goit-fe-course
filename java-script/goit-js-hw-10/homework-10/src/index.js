8632ac5de200cf7013e9e1fd6fbd739d04642986
kristina.lobanets <kristina.malitowska@gmail.com>
2020-07-09 00:14:47 +0300

    kljhg

--- java-script/goit-js-hw-10/homework-10/src/index.js
+++ java-script/goit-js-hw-10/homework-10/src/index.js
@@ -1,1 +1,28 @@
 import './styles.css';
+import array from './menu.json';
+import template from './templates/template.hbs';
+
+const refs = {
+  menuUl: document.querySelector('.js-menu'),
+  switchJs: document.querySelector('.js-switch-input'),
+  body: document.querySelector('body'),
+};
+
+const markUp = () => {
+  const menuLi = array.reduce((acc, item) => acc + template(item), '');
+  refs.menuUl.insertAdjacentHTML('beforeend', menuLi);
+};
+markUp();
+
+const ThemeFunk = event => {
+  if (event.target.checked === true) {
+    refs.body.classList.add('dark-theme');
+    refs.body.classList.remove('light-theme');
+  }
+  if (event.target.checked === false) {
+    refs.body.classList.add('light-theme');
+    refs.body.classList.remove('dark-theme');
+  }
+};
+
+refs.switchJs.addEventListener('change', ThemeFunk);
---
+++ java-script/goit-js-hw-10/homework-10/src/js/markup.js
@@ -0,0 +1,8 @@
+export { markUp };
+// const menuLi = array.map(item => template(item)).join('');
+
+// const menuLi = array.reduce((acc, item) => acc + template(item), '');
+
+// const menuLi = template(array);
+
+// menuUl.insertAdjacentHTML('beforeend', menuLi);
