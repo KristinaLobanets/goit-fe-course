10303090750cd59257feaaa4bd4267da65f28e8a
kristina.lobanets <kristina.malitowska@gmail.com>
2020-07-17 17:37:06 +0300

    ggggg

--- java-script/goit-js-hw-13/src/js/apiService.js
+++ java-script/goit-js-hw-13/src/js/apiService.js
@@ -1,3 +1,20 @@
+const apiKey = '17504115-e1f4917cce04b285bef9ea485';
+const baseUrl = 'https://pixabay.com/api';
+let pageNumber = 1;
+export const apiServiseceFunk = search => {
+  return fetch(
+    `${baseUrl}?image_type=photo&orientation=horizontal&q=${search}&page=${pageNumber}&per_page=5&key=${apiKey}`,
+  )
+    .then(res => {
+      return res.json();
+    })
+    .then(res => res.hits)
+    .then(res => {
+      pageNumber += 1;
+      return res;
+    });
+};
 
-const apiKey = 
-const baseURL = 'https://pixabay.com/api';
+export const resetPage = () => {
+  pageNumber = 1;
+};
